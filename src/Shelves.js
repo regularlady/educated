c94dc37657dd04b4b7038d417dab1b48eac9c520
Brittany Martin <brittany.jill.martin@gmail.com>
2020-03-02 20:54:56 -0500

    type: feat: Create needed components and setup initial routing
    Create routing for "/" and "/search" navigation

---
+++ src/Shelves.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import Bookshelf from './Bookshelf';
+
+class Shelves extends Component {
+
+    state = {
+        shelves: {
+            current: {name: "Currently Reading", books: []},
+            want: {name: "Want to Read", books: []},
+            read: {name: "Read", books: []}
+        }
+    }
+
+
+    render = () => {
+        return (
+            <div>
+                <div className="list-books">
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className="list-books-content">
+                        <div>
+                            <Bookshelf shelf={this.state.shelves.current}/>
+                        </div>
+                    </div>
+                    <div className="open-search">
+                        <Link to='/search'>Add a book</Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Shelves; 
