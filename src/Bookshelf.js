2e5fcb67c34d33f915e1b1da9ec3d69999c099b0
Brittany Martin <brittany.jill.martin@gmail.com>
2020-03-03 20:27:13 -0500

    type: feat: Add state management for choosing where a book belongs

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -10,7 +10,11 @@ class Bookshelf extends Component {
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         <li>
-                            <Book />
+                            <Book
+                                bookTitle='Whistleblower'
+                                bookAuthor='Susan Fowler'
+                                shelfSelection="wantToRead"
+                                backgroundImage='url("http://books.google.com/books/content?id=12OeDwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'/>
                         </li>
                     </ol>
                 </div>
